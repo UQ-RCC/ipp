7cc7e98e8624c6792aff3980bdfdb1751b1efa32
Hoang Nguyen <hoangnguyen177@hotmail.com>
2021-04-16 14:07:05 +1000

    dist to relative path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 const {gitDescribe, gitDescribeSync} = require('git-describe')
 process.env.VUE_APP_GIT_HASH = gitDescribeSync().hash
 process.env.VUE_APP_GIT_TAG = gitDescribeSync().tag
+
+module.exports = {
+    publicPath: './'
+};
+
+// module.exports = {
+//     publicPath: process.env.BASE_URL,
+//     assetsDir: process.env.BASE_URL
+// };
