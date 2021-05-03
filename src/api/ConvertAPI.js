abce67afc46cb92e5d8d86663daa29f72f72ea20
Hoang Nguyen <hoangnguyen177@hotmail.com>
2021-05-04 02:02:46 +1000

    update mem claculation

--- src/api/ConvertAPI.js
+++ src/api/ConvertAPI.js
@@ -7,8 +7,11 @@ export default {
     let endpoint = `${Vue.prototype.$Config.endpoints.pref}`
     // let apihost = /^(?:\w+\:\/\/)?([^\/]+)(.*)$/.exec(endpoint)[1]
     let apihost = /^(?:\w+:\/\/)?([^/]+)(.*)$/.exec(endpoint)[1]
-    // only need to multiple by 2.2, but multiple by 4 to be sure
-    let mem = Math.ceil((maxSize * 4 )/(1024 * 1024 * 1024.0))
+    // only need to multiple by 2.2
+    let mem = Math.ceil((maxSize * 2.2 )/(1024 * 1024 * 1024.0))
+    // maximum mem in a node
+    if(mem > 384)
+      mem = 384
     convertinfo.mem = mem
     const { data } = await request.get(`${Vue.prototype.$Config.endpoints.wiener}/api/execute/convertfilebase64`, {
         params: {
