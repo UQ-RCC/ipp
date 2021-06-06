b3cf575610c28c5722df8fc99f9e9253ed5bdbc1
Hoang Nguyen <hoangnguyen177@hotmail.com>
2021-06-06 22:54:51 +1000

    add option for relpath

--- src/views/Filesmanager.vue
+++ src/views/Filesmanager.vue
@@ -16,6 +16,7 @@
 <script>
     // import * as api from '@/api'
     import FileBrowser from "../components/vuetify-file-browser/";
+    import CollectionsAPI from "@/api/CollectionsAPI";
 
     export default {
         name: 'FilesManager',
@@ -29,13 +30,29 @@
             }
             return data
         },
-        mounted: function() {
-            // console.log(this.$route.query.path)
-            // if(this.$route.query.path){
-            //     // this.$refs.filebrowser.pathChanged("/")
-            //     console.log("@Filemanager chapge path to:" + this.$route.query.path)
-            //     this.$refs.filebrowser.pathChanged(decodeURIComponent(this.$route.query.path))
-            // }
+        mounted: async function() {
+            if(this.$route.query.relpath){
+                console.log(this.$route.query.relpath)
+                var _collectionName = this.$route.query.relpath.split('/')[0]
+                if(_collectionName) {
+                    let _relPatWithoutCollection = this.$route.query.relpath.replace(_collectionName+'/', '')
+                    let _collectionResponse = await CollectionsAPI.list()
+                    _collectionResponse.commandResult.map(_collectionItem => {
+                        let _collectionPath = _collectionItem.output
+                        if(!_collectionPath.endsWith("/")) {
+                            _collectionPath = _collectionPath + '/'
+                        }
+                        if(_collectionPath.includes(_collectionName)){
+                            let _fullPath = _collectionPath + _relPatWithoutCollection
+                            if(!_fullPath.endsWith("/")) {
+                                _fullPath = _fullPath + '/'
+                            }
+                            // console.log("@Filemanager chapge path to:" + _fullPath)
+                            this.$refs.filebrowser.pathChanged(decodeURIComponent(_fullPath))
+                        }
+                    })
+                }
+            }
         },
         methods: {
         }
