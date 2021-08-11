f5878d9c282295c07478a7ec68895cd144675a6a
Hoang Nguyen <hoangnguyen177@hotmail.com>
2021-08-11 22:04:08 +1000

    fix desktop size

--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -19,7 +19,7 @@
                 seamless='seamless'
                 frameborder="0"
                 width="100%"
-                height="100"
+                height="100%"
                 v-if="cookiesReady"
                 v-show="desktopReady"
                 >
