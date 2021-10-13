b1ad2935549cacbd7052e9ec99632e59414a1ad5
hoangnguyen177 <hoangnguyen177@gmail.com>
2021-10-13 15:19:04 +1000

    fix the IPP path to /portal/

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const {gitDescribe, gitDescribeSync} = require('git-describe')
 process.env.VUE_APP_GIT_HASH = gitDescribeSync().hash
 process.env.VUE_APP_GIT_TAG = gitDescribeSync().tag
-// console.log(gitDescribeSync())
-// not work
-// module.exports = {
-//     publicPath: '/portal/'
-// };
-
-// module.exports = {
-//     publicPath: './',
-//     assetsDir: './'
-// };
+module.exports = {publicPath: '/portal/'};
