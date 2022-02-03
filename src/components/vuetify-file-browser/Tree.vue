24ca41d2d6faa182d062837de17aefd01aa2b7d2
hoangnguyen177 <hoangnguyen177@gmail.com>
2022-02-04 05:19:14 +1000

    fix missing collection

--- src/components/vuetify-file-browser/Tree.vue
+++ src/components/vuetify-file-browser/Tree.vue
@@ -50,49 +50,53 @@
             </v-tab-item>
             <v-tab-item key="collections">
                 <v-card flat tile class="d-flex flex-column collections-card">
-                <v-list>
-                    <v-list-item-group color="primary">
-                        <v-list-item
-                            v-for="collection in collections"
-                            :key="collection.name"
-                            @click="changePath(collection.path)"
-                        >
-                        <v-icon color="grey lighten-1">mdi-folder-outline</v-icon>
-                            
-                            <v-list-item-content>
-                                <v-list-item-title  v-text="collection.name"></v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
+                    <div class="scroll-y scroll-x">
+                        <v-list>
+                            <v-list-item-group color="primary">
+                                <v-list-item
+                                    v-for="collection in collections"
+                                    :key="collection.name"
+                                    @click="changePath(collection.path)"
+                                >
+                                <v-icon color="grey lighten-1">mdi-folder-outline</v-icon>
+                                    
+                                    <v-list-item-content>
+                                        <v-list-item-title  v-text="collection.name"></v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list-item-group>
+                        </v-list>
+                    </div>
                 </v-card>
             </v-tab-item>
             <v-tab-item key="bookmarks">
                 <v-card flat tile class="d-flex flex-column bookmark-card">
-                    <v-list-item-group color="primary">
-                        <v-list-item
-                            v-for="bookmark in bookmarks"
-                            :key="bookmark.name"
-                            @click="changePath(bookmark.path)"
-                        >
-                            <v-tooltip top>
-                                <template v-slot:activator="{ on, attrs }">
-                                    <v-btn icon v-bind="attrs" v-on="on">
-                                        <v-icon color="grey lighten-1">mdi-folder-outline</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>{{ bookmark.path }}</span>
-                            </v-tooltip>
-                            
-                            <v-list-item-content>
-                                <v-list-item-title  v-text="bookmark.name"></v-list-item-title>
-                            </v-list-item-content>
+                    <div class="scroll-y scroll-x">
+                        <v-list-item-group color="primary">
+                            <v-list-item
+                                v-for="bookmark in bookmarks"
+                                :key="bookmark.name"
+                                @click="changePath(bookmark.path)"
+                            >
+                                <v-tooltip top>
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-btn icon v-bind="attrs" v-on="on">
+                                            <v-icon color="grey lighten-1">mdi-folder-outline</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>{{ bookmark.path }}</span>
+                                </v-tooltip>
+                                
+                                <v-list-item-content>
+                                    <v-list-item-title  v-text="bookmark.name"></v-list-item-title>
+                                </v-list-item-content>
 
-                            <v-btn icon @click="deleteBookmark(bookmark)">
-                                <v-icon>mdi-delete-outline</v-icon>
-                            </v-btn>
-                        </v-list-item>
-                    </v-list-item-group>
+                                <v-btn icon @click="deleteBookmark(bookmark)">
+                                    <v-icon>mdi-delete-outline</v-icon>
+                                </v-btn>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </div>
                 </v-card>
             </v-tab-item>
         </v-tabs-items>
